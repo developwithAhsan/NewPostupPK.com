f2818fca85ec9f010ed746fe32a9298a7f6e5a60
Muhammad Ahsan <ahsan.ahsan.2977@gmail.com>
2025-12-30 14:02:50 +0500

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -21,15 +21,36 @@ export default defineConfig(({ mode }) => {
         }
       },
       build: {
-        chunkSizeWarningLimit: 1000,
+        target: 'es2015',
+        minify: 'esbuild',
+        chunkSizeWarningLimit: 500,
         rollupOptions: {
           output: {
             manualChunks: {
-              'vendor-react': ['react', 'react-dom', 'react-router-dom'],
+              'vendor-react': ['react', 'react-dom'],
+              'vendor-router': ['react-router-dom'],
               'vendor-icons': ['lucide-react'],
-            }
-          }
-        }
+              'vendor-helmet': ['react-helmet-async'],
+            },
+            assetFileNames: (assetInfo) => {
+              const info = assetInfo.name.split('.');
+              const ext = info[info.length - 1];
+              if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
+                return `assets/images/[name]-[hash][extname]`;
+              }
+              return `assets/[name]-[hash][extname]`;
+            },
+            chunkFileNames: 'js/[name]-[hash].js',
+            entryFileNames: 'js/[name]-[hash].js',
+          },
+        },
+        cssCodeSplit: true,
+        assetsInlineLimit: 4096,
+        reportCompressedSize: false,
+        sourcemap: false,
+      },
+      optimizeDeps: {
+        include: ['react', 'react-dom', 'react-router-dom', 'lucide-react'],
       }
     };
 });
